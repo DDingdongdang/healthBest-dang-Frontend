fbeac85f6540099c39ddc8ed698c7dad61f0b03e
yerin03 <kyr2532@naver.com>
2023-05-17 20:41:52 +0900

    write foodname

---
+++ dangdiary/app/src/main/java/com/example/dangdiary/WriteFood.java
@@ -0,0 +1,20 @@
+package com.example.dangdiary;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class WriteFood extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //layout 넘어오기
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.writefood);
+        Intent intent = getIntent(); }
+
+
+}
