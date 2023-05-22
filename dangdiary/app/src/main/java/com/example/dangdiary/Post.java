376a0a9d68ba8b5ae5746d3925542ff20c41d7ac
yerin03 <kyr2532@naver.com>
2023-05-22 16:33:14 +0900

    rest api

---
+++ dangdiary/app/src/main/java/com/example/dangdiary/Post.java
@@ -0,0 +1,21 @@
+package com.example.dangdiary;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+
+    private long id;
+    private String title;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getContent() {
+        return content;
+    }
+
+}
--- dangdiary/app/src/main/java/com/example/dangdiary/WriteFood.java
+++ dangdiary/app/src/main/java/com/example/dangdiary/WriteFood.java
@@ -1,13 +1,26 @@
+/*
 package com.example.dangdiary;
 
 import android.content.Intent;
 import android.os.Bundle;
+//import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class WriteFood extends AppCompatActivity {
+
+    TextView str_info; //받아올 정보
+    EditText str_foodname; //전송할 음식 이름
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +29,71 @@ public class WriteFood extends AppCompatActivity {
         setContentView(R.layout.writefood);
         Intent intent = getIntent();
 
+        str_info = (TextView)findViewById(R.id.str_info);
+        str_foodname = (EditText)findViewById(R.id.str_foodname);
     }
 
+
 }
+*/
+
+package com.example.dangdiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.widget.TextView;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+//import com.example.dangdiary.Post;
+
+public class WriteFood extends AppCompatActivity {
+    private TextView textViewResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.writefood);
+        Intent intent = getIntent();
+        textViewResult = findViewById(R.id.text_view_result); //결과가 나올 txt view
+        Retrofit retrofit = new Retrofit.Builder() //retorfit 인스턴스 생성
+                .baseUrl("3.39.11.35:8080")//서버를 돌릴 ip주소 : port번호
+                .addConverterFactory(GsonConverterFactory.create()) //json 데이터를 자바 객체로 변환
+                .build(); //Retrofit인스턴스를 만들고 반환
+
+        RestApi jsonPlaceHolderApi = retrofit.create(RestApi.class); //restapi 인스턴스 생성
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(); //서버에 대한 http get요청을 나타내는 call 객체 생성
+        //restapi에서 정의했던 getPosts 메서드를 호출
+        call.enqueue(new Callback<List<Post>>() { //enqueue() 메서드는 비동기적으로 요청을 실행(응답 기다리지X)
+            //Callback<List<Post>> -> 비동기적인 http 요청결과를 처리하는 메서드 정의
+
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+                List<Post> posts = response.body(); //응답을 처리하는 부분 List<post>형식으로 반환
+                for (Post post : posts) { //post목록에 있는 각 게시물을 가져온다
+                    String content = "";
+                    content += "ID: " + post.getId() + "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+                    content += "Content: " + post.getContent() + "\n\n";
+                    textViewResult.append(content); //content에 추가한다.
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+}
+
