416f5008b8bc9a8d64df92e8aaf7ef58f025940e
yerin03 <kyr2532@naver.com>
2023-05-28 20:29:44 +0900

    UserCreateResponse

--- dangdiary/app/src/main/java/com/example/dangdiary/auth/java/Join.java
+++ dangdiary/app/src/main/java/com/example/dangdiary/auth/java/Join.java
@@ -1,24 +1,15 @@
-package com.example.dangdiary.menu;
+package com.example.dangdiary.auth.java;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
-import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.toolbox.Volley;
 import com.example.dangdiary.R;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class Join extends AppCompatActivity {
 
